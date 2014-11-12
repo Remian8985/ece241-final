8ece2aab2a3f57a56d7752f7806d6cd423f8858e
Tasdiq Ameem <ameem.is.online@gmail.com>
2014-11-11 23:11:37 -0500

    ary had a little lamb

--- pieces/sub_matrix.v
+++ pieces/sub_matrix.v
@@ -5,11 +5,11 @@ module sub_matrix (clk, data_in, clk, resetn, data_out);
 	input clk, resetn; 
 	output reg [15:0] data_out;
 
-	always @(posedge clk or posedge resetn) begin
-		if (resetn) begin
+	always @(posedge clk ) begin
+		if (!resetn) begin
 			data_in <= 16'b0;			
 		end
-		else if (clk) begin
+		else begin
 			data_out <= data_in;
 		end
 	end
